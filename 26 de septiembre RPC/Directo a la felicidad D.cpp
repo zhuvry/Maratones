f02389241f75c712808ba13f597a4535012ca814
zhuvry <briangiraldo123@hotmail.com>
2015-10-05 19:05:29 -0500

    borrado por titulo no serio

--- 26 de septiembre RPC/Directo a la felicidad D.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long int lli;
-lli N;
-int main(){
-}
--- codeforces 24 junio/A.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-string s;
-int main()
-{
-    while(cin>>s){
-        cout<<(s.size()+1)*26-s.size()<<endl;
-    }
-}
